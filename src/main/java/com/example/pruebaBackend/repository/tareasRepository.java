f9501b2b8b0cb5d31fb8c58256f0d2749c70f359
Hernan-Mejia <79726301+Hernan-Mejia@users.noreply.github.com>
2022-01-20 09:53:00 -0500

    Update tareasRepository.java

--- src/main/java/com/example/pruebaBackend/repository/tareasRepository.java
+++ src/main/java/com/example/pruebaBackend/repository/tareasRepository.java
@@ -2,9 +2,9 @@ package com.example.pruebaBackend.repository;
 
 import com.example.pruebaBackend.models.TareasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
-
+@Repository
 public interface tareasRepository extends JpaRepository<TareasModel, Long> {
     List<TareasModel> findAllById(Long idUsuario);
 }
